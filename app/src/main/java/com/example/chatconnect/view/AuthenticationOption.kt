c552690066b2a8b3fb68a8856b7d8eb83f9f03d9
Anurag Mishra <anurag026mishra@gmail.com>
2023-11-04 17:12:29 +0530

    chatConnect

---
+++ app/src/main/java/com/example/chatconnect/view/AuthenticationOption.kt
@@ -0,0 +1,40 @@
+package com.example.chatconnect.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.chatconnect.ui.theme.FlashChatTheme
+
+/**
+ * The authentication view which will give the user an option to choose between
+ * login and register.
+ */
+
+@Composable
+fun AuthenticationView(register: () -> Unit, login: () -> Unit) {
+    FlashChatTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Title(title = "⚡️ Chat Connect")
+                Buttons(title = "Register", onClick = register, backgroundColor = Color.Blue)
+                Buttons(title = "Login", onClick = login, backgroundColor = Color.Magenta)
+            }
+        }
+    }
+}

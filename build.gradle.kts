b14207036410a90740a513966f7067befaa635b3
Anurag Mishra <anurag026mishra@gmail.com>
2023-11-04 12:30:51 +0530

    chatConnect

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,21 @@
 buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath ("com.android.tools.build:gradle:7.0.0")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath ("com.google.gms:google-services:4.3.8")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
     }
 }
+
+
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.1" apply false
